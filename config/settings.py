37d0104a3bd5c39d19227dda45af3d015b09354c
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-02-23 22:57:41 +0400

    ana sehife strukturu yaradildi, css, js static file-dan temin edildi

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ BASE_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
-    
+
 ]
 
 MY_APPS = [
@@ -65,7 +65,9 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / 'templates',
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_files',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
