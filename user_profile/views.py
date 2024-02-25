dbb8eca9b3a7956e6f60590e89d97c728ba3dce8
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-02-25 22:48:37 +0400

    login olunmus istifadecinin ana sehifeye gede bilmesi temin edildi

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
 
 def login_view(request):
     # login olan istifadeci birbasa ana sehifeye getsin
     context = dict()
+    if request.method == 'POST':
+        # print(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        # Bu bilgilerin dogru alinib alinmadigini kontrol edelim
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # login oldugunu istifadeciye belli edelim!
+            return redirect('home_view')
     return render(request, 'user_profile/login.html', context)
