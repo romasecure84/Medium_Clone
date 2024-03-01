79417df8c2fb56ae9352a695d6d65734d6671758
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-03-01 19:15:23 +0400

    easy-thumnails elave olundu, settingsde qurasdirildi

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ BASE_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
+    'easy_thumbnails',
     'tinymce',
     "crispy_forms",
     "crispy_bootstrap5",
@@ -165,3 +166,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CRISPY:
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+THUMBNAIL_ALIASES = {
+    '': {
+        'avatar': {'size': (30, 30), 'crop': True},
+        'avatar2x': {'size': (60, 60), 'crop': True},
+        'thumbnail': {'size': (400, 300), 'crop': True},
+        'page': {'size': (800, 450), 'crop': True},
+
+    },
+}
