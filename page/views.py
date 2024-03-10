0ba23960e2f8238c51307157151e415ddf1670ed
Rafail Jabrayilov <romasecure84@yandex.ru>
2024-03-10 23:39:30 +0400

    Bezi postlar elave olundu

--- page/views.py
+++ page/views.py
@@ -3,11 +3,13 @@ from blog.models import BlogPost, Tag, Category
 
 def home_view(request):
     posts = BlogPost.objects.filter(is_active=True) #.order_by('-created_at')
+    top_posts = posts.order_by('-view_count')[:6]
     tags = Tag.objects.filter(is_active=True)
     categories = Category.objects.filter(is_active=True)
     context = dict(
         posts=posts,
         tags = tags,
         categories = categories,
+        top_posts = top_posts,
     )
     return render(request, 'page/home_page.html', context)
